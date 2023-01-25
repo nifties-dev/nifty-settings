1d007da18e6d8ad826946ffeb682721a192f3c84
drone <drone@substance.org.lv>
2023-01-25 13:20:38 +0000

    initial generated app

---
+++ src/test/java/dev/nifties/settings/QuickletSettingsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.nifties.settings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickletSettingsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
