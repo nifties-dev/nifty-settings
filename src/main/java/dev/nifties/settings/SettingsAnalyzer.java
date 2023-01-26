bc837bf4582d0e67a1e5ea33e83dc42d978a214d
Andris Rauda <andris.rauda@substance.org.lv>
2023-01-26 19:29:25 +0200

    minimum useCase2

--- src/main/java/dev/nifties/settings/SettingsAnalyzer.java
+++ src/main/java/dev/nifties/settings/SettingsAnalyzer.java
@@ -1,20 +1,27 @@
 package dev.nifties.settings;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Objects;
+import java.util.function.Function;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import dev.nifties.settings.annotation.Setting;
 
 public class SettingsAnalyzer {
 
     public Collection<SettingAccessor> get(Class<?> clazz) {
-        return Arrays.stream(clazz.getDeclaredFields())
-                .map(f -> this.processField(clazz, f)).filter(Objects::nonNull)
-                .collect(Collectors.toList());
+        return Stream
+                .concat(Arrays.stream(clazz.getDeclaredFields())
+                        .map(f -> this.processField(clazz, f)),
+                        Arrays.stream(clazz.getDeclaredMethods())
+                                .map(m -> this.processMethod(clazz, m)))
+                .filter(Objects::nonNull).collect(Collectors.toList());
     }
 
     protected SettingAccessor processField(Class<?> clazz, Field field) {
@@ -28,6 +35,45 @@ public class SettingsAnalyzer {
                 o -> this.getter(field, o), (o, v) -> this.setter(field, o, v));
     }
 
+    protected SettingAccessor processMethod(Class<?> clazz, Method method) {
+        Setting settingAnnotation = method.getAnnotation(Setting.class);
+        if (settingAnnotation == null) {
+            return null;
+        }
+        if (!method.getName().startsWith("set")
+                || method.getName().length() <= 4
+                || method.getParameterCount() != 1) {
+            throw new IllegalArgumentException(
+                    "@Settings not allowed on method: " + method + ","
+                            + " only setters and fields can be marked with this annotation");
+        }
+        // makeAccessible(method);
+
+        String fieldName = Character.toLowerCase(method.getName().charAt(3))
+                + method.getName().substring(4);
+
+        Function<Object, Object> getter;
+        try {
+            Field field = clazz.getField(fieldName);
+            getter = o -> this.getter(field, o);
+        } catch (NoSuchFieldException e) {
+            try {
+                String getterName = (Boolean.TYPE
+                        .isAssignableFrom(method.getParameterTypes()[0])
+                        || Boolean.class.isAssignableFrom(
+                                method.getParameterTypes()[0]) ? "is" : "get")
+                        + method.getName().substring(3);
+
+                Method getterMethod = clazz.getMethod(getterName);
+                getter = o -> this.getter(getterMethod, o);
+            } catch (NoSuchMethodException e1) {
+                getter = o -> null;
+            }
+        }
+        return new SettingAccessor(clazz.getName() + '.' + fieldName, getter,
+                (o, v) -> this.setter(method, o, v));
+    }
+
     protected Object getter(Field field, Object object) {
         try {
             return field.get(object);
@@ -36,10 +82,26 @@ public class SettingsAnalyzer {
         }
     }
 
+    protected Object getter(Method method, Object object) {
+        try {
+            return method.invoke(object);
+        } catch (IllegalAccessException | InvocationTargetException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
     protected void setter(Field field, Object object, Object value) {
         try {
             field.set(object, value);
         } catch (IllegalAccessException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    protected void setter(Method method, Object object, Object value) {
+        try {
+            method.invoke(object, value);
+        } catch (IllegalAccessException | InvocationTargetException e) {
             throw new IllegalStateException(e);
         }
     }
--- src/test/java/dev/nifties/settings/QuickletSettingsUsecaseTests.java
+++ src/test/java/dev/nifties/settings/QuickletSettingsUsecaseTests.java
@@ -1,9 +1,13 @@
 package dev.nifties.settings;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.io.Closeable;
 import java.util.Collection;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
 
 import org.junit.jupiter.api.Test;
 
@@ -32,7 +36,7 @@ public class NiftySettingsUsecaseTests {
     }
 
     @Test
-    public void simpleToggleService() {
+    public void useCase1() {
         settingsService.put(MyService1.class.getName() + ".enabled",
                 Boolean.TRUE);
 
@@ -44,5 +48,54 @@ public class NiftySettingsUsecaseTests {
         settingsService.remove(MyService1.class.getName() + ".enabled");
         settingsManager.inject(myService);
         assertFalse(myService.isEnabled());
+    }
+
+    public class MyService2 implements Closeable {
+        private ThreadPoolExecutor threadPoolExecutor = (ThreadPoolExecutor) Executors
+                .newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+
+        public ThreadPoolExecutor getThreadPoolExecutor() {
+            return threadPoolExecutor;
+        }
+
+        public int getThreadPoolSize() {
+            return threadPoolExecutor.getCorePoolSize();
+        }
+
+        @Setting
+        public void setThreadPoolSize(int size) {
+            threadPoolExecutor.setMaximumPoolSize(size);
+            threadPoolExecutor.setCorePoolSize(size);
+        }
+
+        public void close() {
+            threadPoolExecutor.shutdown();
+        }
+    }
+
+    @Test
+    public void useCase2() {
+        int defaultPoolSize = Runtime.getRuntime().availableProcessors();
+        int customPoolSize = defaultPoolSize * 2;
+        settingsService.put(MyService2.class.getName() + ".threadPoolSize",
+                customPoolSize);
+
+        try (MyService2 myService = new MyService2()) {
+            settingsManager.inject(myService);
+            assertEquals(customPoolSize,
+                    myService.getThreadPoolExecutor().getCorePoolSize());
+            assertEquals(customPoolSize,
+                    myService.getThreadPoolExecutor().getMaximumPoolSize());
+        }
+
+        try (MyService2 myService = new MyService2()) {
+            settingsService
+                    .remove(MyService2.class.getName() + ".threadPoolSize");
+            settingsManager.inject(myService);
+            assertEquals(defaultPoolSize,
+                    myService.getThreadPoolExecutor().getCorePoolSize());
+            assertEquals(defaultPoolSize,
+                    myService.getThreadPoolExecutor().getMaximumPoolSize());
+        }
     }
 }
