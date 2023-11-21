0415416a120e749472255ba61a8be31fcbc7e6f9
Andris Rauda <andris.rauda@substance.org.lv>
2023-11-21 13:24:10 +0200

    default SettingsBinder uses IdentityHashMap

--- src/main/java/dev/nifties/settings/SettingsBinder.java
+++ src/main/java/dev/nifties/settings/SettingsBinder.java
@@ -1,13 +1,16 @@
 package dev.nifties.settings;
 
 import java.util.Collection;
+import java.util.Collections;
+import java.util.IdentityHashMap;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Consumer;
 
 public class SettingsBinder {
 
-    private final Map<Object, Collection<Consumer<SettingValue>>> bindings = new ConcurrentHashMap<>();
+    private final Map<Object, Collection<Consumer<SettingValue>>> bindings =
+            Collections.synchronizedMap(new IdentityHashMap<>());
 
     public void add(Object object, Collection<Consumer<SettingValue>> listeners) {
         bindings.put(object, listeners);
