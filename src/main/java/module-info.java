6faa5b631a192d8f43ec42a5166a73519b621f28
Andris Rauda <andris.rauda@substance.org.lv>
2024-05-07 22:42:36 +0300

    use requires static for lombok

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,7 @@ import dev.nifties.settings.SettingsChannel;
 import dev.nifties.settings.SettingsSource;
 
 module nifty.settings.client {
-    requires lombok;
+    requires static lombok;
     exports dev.nifties.settings;
     exports dev.nifties.settings.annotation;
     uses SettingsSource;
